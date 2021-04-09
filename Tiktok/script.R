11393100dc50dc5df7d2470ad7755ace3f9a754a
unknown <jacek.pardyak@gmail.com>
2021-04-09 23:27:25 +0200

    clean

---
+++ Tiktok/script.R
@@ -0,0 +1,16 @@
+if(!require(remotes)) {
+  install.packages("remotes"); require(remotes)}
+remotes::install_github("gadenbuie/tiktokrmd")
+library(tiktokrmd)
+
+
+tt_url <- "https://www.tiktok.com/@chelseaparlettpelleriti/video/6811647290709757189"
+tt <- tiktok_embed(tt_url)
+tt
+
+# Or write a plain markdown version
+tiktok_md(tt)
+#> [1] "![[#cake #cakes #dontmixit #cakelover #cakelovers #dumpcake #food #tiktokfood #foodtiktok #easyrecipe #easyrecipes #peach #peaches #cinnamon #fyp](https://www.tiktok.com/@aquickspoonful/video/6890681375431691526) by [Sophia Wasu](https://www.tiktok.com/@aquickspoonful)](https://p16-sign-va.tiktokcdn.com/obj/tos-maliva-p-0068/00189a8b703343bd817a3ccaec240f71?x-expires=1610654400&x-signature=ExJZaEMAgxhyXydfgsqgZqU%2B8y8%3D)"
+
+# Or as HTML without the full TikTok embedded player shenanigans
+tiktok_html(tt, include_player = FALSE)
