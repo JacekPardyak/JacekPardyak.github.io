ac6569de82988623d55e25ec77c802bed84e1293
unknown <jacek.pardyak@gmail.com>
2022-07-24 12:40:33 +0200

    version 2

--- inkscaper/graphics.R
+++
@@ -1,9 +0,0 @@
-# Way to convert svg files to pdf and png
-# requires Inkscape installed
-
-files <- list.files("./F/", pattern = ".svg", full.names = T)
-
-for (file in files) {
-   command <- paste('inkscape  --export-type=png --export-width=30', file, sep = " ")
-   system(command)
-}
